81d53a77ba01608b4eef965c3236d7a686e491fa
sed-jackob <70657965+sed-jackob@users.noreply.github.com>
2021-07-31 17:06:21 -0600

    updated config

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_key = "6358542c8dmsh4b07038f2b6d052p1898fajsne9fbc58d30f6" #Mohammad API
+# API_key = "6358542c8dmsh4b07038f2b6d052p1898fajsne9fbc58d30f6" #Mohammad API
 API_key = "0bc6d85258msh0169da777118572p18eaf1jsnfdb23e2857d9" #Saeed API
 db_pass = "Odiido76"
