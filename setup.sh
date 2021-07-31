9d71e6fab52a0a2bd093f235c09f8c592fd6459d
sed-jackob <70657965+sed-jackob@users.noreply.github.com>
2021-07-31 17:26:22 -0600

    updated files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit/
+echo “\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-" > ~/.streamlit/config.toml
+” > ~/.streamlit/config.toml
